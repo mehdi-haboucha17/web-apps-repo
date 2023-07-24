c5fe7ecda6b15987485484c154f750c508616991
Mehdi Haboucha <mehdi.haboucha@bankofafricauk.co.uk>
2023-07-24 17:38:43 +0100

    feat: layout middle panel with status bar and upload selection

--- facebook-clone/src/components/mainpage/left-side-panel/leftSidePanel.js
+++ facebook-clone/src/components/mainpage/left-side-panel/leftSidePanel.js
@@ -4,8 +4,7 @@ import ImageLayout from "./imageLayout";
 import covid from "../../../images/covid.png";
 import group from "../../../images/groups.png";
 import memories from "../../../images/memories.png";
-import messengerKids from "../../../images/messenger.svg";
-import blood from "../../../images/feelings.png";
+
 const LeftSidePanel = () => {
   const [data, setdata] = useState([]);
   const getData = () => {
@@ -13,16 +12,7 @@ const LeftSidePanel = () => {
       { image: covid, text: "Covid-19 Information Centre" },
       { image: group, text: "Friends" },
       { image: memories, text: "Memories" },
-      //   { image: messengerKids, text: "Messenger Kids" },
-      //   { image: "", text: "Ad Centre" },
-      //   { image: "", text: "Ads Manager" },
-      //   { image: blood, text: "Blood Donation" },
-      //   { image: "", text: "Business Manager" },
-      //   { image: "", text: "Climate Science Information Centre" },
-      //   { image: "", text: "Community Help" },
-      //   { image: "", text: "Events" },
-      //   { image: "", text: "Films" },
-      //   { image: "", text: "Friend Lists" },
+      //
     ];
     setdata(jsonData);
   };
